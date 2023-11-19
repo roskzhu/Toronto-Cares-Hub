704a5d9392467f88852037eecbc959e9307dac0e
roskzhu <roskenzhu@gmail.com>
2023-11-19 02:33:53 -0500

    db

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect('mongodb://localhost:27017/your-database-name', {
+mongoose.connect('mongodb://localhost:27017/metrics', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
